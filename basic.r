a471ba682f2037a29f5652f16db9940c7f6a857a
johnpinney <56346625+johnpinney@users.noreply.github.com>
2021-10-20 11:54:55 +0100

    added r commands

---
+++ basic.r
@@ -0,0 +1,61 @@
+
+### LOADING DATA
+
+# Load a table of data from a CSV file
+iris <- read.csv( "iris.csv" )
+
+# See the variables (column names) that are present in the table
+str( iris )
+
+# Get hold of a named column as a data vector
+iris$sepal.width
+
+
+### DISPLAYING DATA
+
+# Load the datasets
+titanic <- read.csv( "titanic.csv" )
+iris <- read.csv( "iris.csv" )
+
+# A frequency table
+table( titanic$status )
+
+# A bar chart
+barplot( table( titanic$status ) )
+
+# A histogram
+hist( iris$petal.length, main="My Histogram", xlab="Petal length /cm" )
+
+# A scatter plot
+plot( iris$petal.length, iris$petal.width, xlab="Petal length /cm", ylab="Petal width /cm" )
+
+# Add a regression line
+abline( lm( iris$petal.width ~ iris$petal.length ), col="red" )
+
+
+### DESCRIPTIVE STATISTICS
+
+# The mean
+mean( iris$sepal.length )
+
+# The median
+median( iris$sepal.length )
+
+# A function to get the mode
+getmode <- function(v) {
+   uniqv <- unique(v)
+   uniqv[which.max(table(match(v, uniqv)))]
+}
+getmode( titanic$status )
+
+# The interquartile range
+IQR( iris$sepal.length )
+
+# A box plot
+boxplot( iris$sepal.length, horizontal=TRUE, xlab="Sepal length /cm" )
+
+# The variance
+var( iris$sepal.length )
+
+# The standard deviation
+std( iris$sepal.length )
